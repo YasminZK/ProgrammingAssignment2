ecf8f3b746ea96cca0520f1e6e669470cc5ce6d0
YasminZK <y.zidour-khan@outlook.com>
2025-06-26 15:25:53 +0100

    Completed Programming Assignment 2: Caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,81 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
+## These functions cache the inverse of a matrix to avoid redundant calculations.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # 'inv' will store the cached inverse
+  
+  # Setter function: update the matrix and clear cached inverse
+  set <- function(y) {
+    x <<- y       # assign new matrix to x in parent env
+    inv <<- NULL  # reset cached inverse because matrix changed
+  }
+  
+  # Getter function: returns the matrix
+  get <- function() x
+  
+  # Setter for inverse: store the inverse matrix in cache
+  setInverse <- function(inverse) inv <<- inverse
+  
+  # Getter for inverse: return cached inverse matrix
+  getInverse <- function() inv
+  
+  # Return a list of the above functions so they can be called externally
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()  # try to get cached inverse
+  
+  # If cached inverse exists, return it with a message
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # Otherwise, get the matrix data
+  data <- x$get()
+  
+  # Calculate the inverse using solve()
+  inv <- solve(data, ...)
+  
+  # Cache the inverse for future calls
+  x$setInverse(inv)
+  
+  # Return the inverse matrix
+  inv
+}
+## Functions for caching matrix inverse
 
-
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) {
+  # ... your function code here ...
+}
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # ... your function code here ...
 }
+
+## Testing the caching functions
+
+# Create a matrix
+mat <- matrix(c(2, 0, 0, 2), nrow = 2, ncol = 2)
+
+# Create the special cached matrix object
+cachedMat <- makeCacheMatrix(mat)
+
+# Calculate inverse first time (should compute and cache)
+inv1 <- cacheSolve(cachedMat)
+print(inv1)
+
+# Calculate inverse second time (should get cached and print message)
+inv2 <- cacheSolve(cachedMat)
+print(inv2)
+
+# Change the matrix to a new one
+cachedMat$set(matrix(c(1, 2, 3, 4), 2, 2))
+
+# Calculate inverse again (should compute fresh because matrix changed)
+inv3 <- cacheSolve(cachedMat)
+print(inv3)
